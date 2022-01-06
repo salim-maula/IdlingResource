cf10f578d45a5767bb0bbe9ae692a959b42ba926
salim-maula <salim-maula>
2022-01-06 18:03:21 +0700

    first commit

---
+++ app/src/main/java/com/example/myidlingresource/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myidlingresource
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myidlingresource.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.button.setOnClickListener {
+            delay1()
+        }
+    }
+
+    private fun delay1() {
+        EspressoIdlingResource.increment()
+        Handler(Looper.getMainLooper()).postDelayed({
+            activityMainBinding.textView.text = getString(R.string.delay1)
+            if (!EspressoIdlingResource.getEspressoIdlingResource().isIdleNow) {
+                //Memberitahukan bahwa tugas sudah selesai dijalankan
+                EspressoIdlingResource.decrement()
+            }
+        }, 2000)
+    }
+
+}
